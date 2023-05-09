93e16fe374b389c97e7b818ee156945f74e51020
Guilherme <guilhermemagno09@gmail.com>
2023-05-09 13:25:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String result = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () async {
+                result = '';
+                setState(() {
+                  result = result;
+                });
+                try {
+                  final response = await getData();
+                  setState(() {
+                    result = response.toString();
+                  });
+                } catch (e) {
+                  const error = 'an error ocurred';
+                  setState(() {
+                    result = error;
+                  });
+                }
+              },
+            ),
+            const Spacer(),
+            Text(result.toString()),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future getData() async {
+  final dio = Dio();
+  final response =
+      await dio.get('https://official-joke-api.appspot.com/random_joke');
+  return response;
+}
