d5e9ba4a71085934219359515016c7ef57b5e2df
Guilherme <guilhermemagno09@gmail.com>
2023-05-09 15:19:06 -0300

    creating profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
+import 'profile_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage());
+      title: 'Cookbook',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProfileScreen(),
+    );
   }
 }
 
